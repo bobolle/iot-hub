0c515ab51e337e5eab266d046273a6a56f4674fe
bobolle <botdahlstrom@gmail.com>
2025-02-21 10:23:03 +0100

    stores data from msgqueue to db

---
+++ exec_db.sh
@@ -0,0 +1,1 @@
+sudo docker exec -it db psql -d db -U db_admin
--- hub.py
+++ hub.py
@@ -4,13 +4,23 @@ import time
 
 def on_connect(client, userdata, flags, reason_code, properties):
     if reason_code.is_failure:
-        print(f"connected with result code {reason_code}")
+        print(f"connection to mqtt-broker returned with result code {reason_code}")
     else:
-        #client.subscribe("$SYS/#")
-        print(f"connected")
+        print(f"connected to mqtt-broker")
 
 def on_message(client, userdata, msg):
-    print(f"message received: {msg.payload.decode()}")
+    try:
+        data = msg.payload.decode()
+        print(f"message received: {data}")
+
+        cur = conn.cursor()
+        sql = f"INSERT INTO device (name) VALUES ('{data}')"
+        cur.execute(sql)
+        conn.commit()
+
+        print(f"stored in db: {data}")
+    except Exception as e:
+        print(f"error on_message: {e}")
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_connect = on_connect
@@ -20,5 +30,9 @@ mqttc.connect("broker", 1883, 60)
 mqttc.subscribe("pico/sensor/distance")
 
 conn = psycopg2.connect("dbname=db user=db_admin password=1234 host=db port=5432")
+
+# from msgqueue, fetch and decode msg
+# insert to db
+# every 50 inserts, calc avg. , convert to json, make put req
 
 mqttc.loop_forever()
