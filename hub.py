db4f96d090c48c4ba55f7e931e281b6173ac9509
bobolle <botdahlstrom@gmail.com>
2025-03-06 14:52:48 +0100

    feat: disconnect device

--- hub.py
+++ hub.py
@@ -3,11 +3,16 @@ import psycopg2
 import time
 import requests
 
-def on_connect(client, userdata, flags, reason_code, properties):
-    if reason_code.is_failure:
-        print(f"connection to mqtt-broker returned with result code {reason_code}")
+def on_connect(client, userdata, flags, result_code, properties):
+    if result_code.is_failure:
+        print(f"connection to mqtt-broker returned with result code {result_code}")
     else:
         print(f"connected to mqtt-broker")
+        mqttc.subscribe("pico/sensor/distance")
+        # send device_id to cloud in order to manage connection
+
+def on_disconnect(client, userdata, result_code):
+    print(f"disconnected with result code {result_code}")
 
 def on_message(client, userdata, msg):
     try:
@@ -19,14 +24,16 @@ def on_message(client, userdata, msg):
         cur.execute(sql)
         conn.commit()
 
-        print(f"stored in db: {data}")
-
         send_to_cloud('/post/device', data)
+
     except Exception as e:
         print(f"error on_message: {e}")
 
 def calc_avg():
     return avg;
+
+def disconnect_device(client, device_id):
+    client.publish(f"edge-device/{device-id}/disconnect", "1")
 
 def send_to_cloud(path, data):
     url = 'http://192.168.1.13:9090/api' + path
@@ -34,20 +41,14 @@ def send_to_cloud(path, data):
     json_data = {'name': data}
 
     r = requests.post(url, headers=headers, json=json_data)
-
-    return 1
+    return r
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
 
 mqttc.connect("broker", 1883, 60)
-mqttc.subscribe("pico/sensor/distance")
 
 conn = psycopg2.connect("dbname=db user=db_admin password=1234 host=db port=5432")
-
-# from msgqueue, fetch and decode msg
-# insert to db
-# every 50 inserts, calc avg. , convert to json, make put req
 
 mqttc.loop_forever()
