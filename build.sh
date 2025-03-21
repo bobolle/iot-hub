6e2dbb561a57d725dc8d447e9fd94d0e3f87286f
bobolle <botdahlstrom@gmail.com>
2025-03-21 15:59:15 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-if [ "$#" -lt 3 ]; then
-    echo "Invalid format: example $0 <db_name> <db_user> <db_pass>"
-    exit 1
-fi
-
 echo "Building iot-hub ..."
 
-if [ "$#" = 3 ]; then
+DEFAULT_DB_NAME="db"
+DEFAULT_DB_USER="db_admin"
+DEFAULT_DB_PASS="1234"
 
-    DB_NAME=$1
-    DB_USER=$2
-    DB_PASS=$3
-    PACKAGES="python3 python3-pip python3-venv mosquitto mosquitto-clients postgresql"
+read -p "Enter name of database [$DEFAULT_DB_NAME]: " DB_NAME
+DB_NAME=${DB_NAME:-$DEFAULT_DB_NAME}
 
-    apt-get install $PACKAGES -y &&
-    cp ./mosquitto.conf /etc/mosquitto/mosquitto.conf &&
-    systemctl start mosquitto.service &&
-    systemctl start postgresql.service &&
-    { sudo -u postgres psql <<EOF
-        CREATE USER $DB_USER WITH PASSWORD '$DB_PASS';
-        CREATE DATABASE $DB_NAME WITH OWNER '$DB_USER'; 
-        GRANT ALL PRIVILEGES ON DATABASE $DB_NAME to $DB_USER;
+read -p "Enter user for database [$DEFAULT_DB_USER]: " DB_USER
+DB_USER=${DB_USER:-$DEFAULT_DB_USER}
+
+read -s -p "Enter password for user [$DEFAULT_DB_PASS]: " DB_PASS
+echo
+DB_PASS=${DB_PASS:-$DEFAULT_DB_PASS}
+
+PACKAGES="python3 python3-pip python3-venv mosquitto mosquitto-clients postgresql"
+
+apt-get install $PACKAGES -y &&
+cp ./mosquitto.conf /etc/mosquitto/mosquitto.conf &&
+systemctl start mosquitto.service &&
+systemctl start postgresql.service &&
+{ sudo -u postgres psql <<EOF
+    CREATE USER $DB_USER WITH PASSWORD '$DB_PASS';
+    CREATE DATABASE $DB_NAME WITH OWNER '$DB_USER'; 
+    GRANT ALL PRIVILEGES ON DATABASE $DB_NAME to $DB_USER;
 EOF
-    } && echo "Done"
-fi
+} && echo "Done"
