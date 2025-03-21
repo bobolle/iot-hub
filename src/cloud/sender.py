1901309bb106c56441c70c35969249e70a1367f3
bobolle <botdahlstrom@gmail.com>
2025-03-21 20:37:57 +0100

    fix: format of cloud adress and port

--- src/cloud/sender.py
+++ src/cloud/sender.py
@@ -1,7 +1,7 @@
 import requests
 
 def send_to_cloud(path, data):
-    url = 'http://192.168.1.13/9090/api' + path
+    url = 'http://192.168.1.13:9090/api' + path
     headers = {'Content-Type': 'application/json'}
 
     try:
