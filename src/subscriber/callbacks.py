5b2dc3c399bb02cdff92fc21eac60ad41f04f3a8
bobolle <botdahlstrom@gmail.com>
2025-03-21 16:00:49 +0100

    no parse of json data just sending it through

--- src/subscriber/callbacks.py
+++ src/subscriber/callbacks.py
@@ -10,6 +10,8 @@ def on_message(client, userdata, msg):
     device_type = topics[0]
     device_id = topics[1]
 
+    #create funciton for sending data to db
+
     #try:
     #    //cursor = conn.cursor()
     #    //cursor.execute()
@@ -17,5 +19,5 @@ def on_message(client, userdata, msg):
     #except Exception as e:
     #    print(f"error: on_message, {e}")
 
-    payload = json.loads(msg.payload.decode())
-    send_to_cloud('/data', payload)
+    #payload = json.loads(msg.payload.decode())
+    send_to_cloud('/data', msg.payload)
