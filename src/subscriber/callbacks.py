d3a3e6853b415e33c1fbfd0153e741d7ad1b301b
bobolle <botdahlstrom@gmail.com>
2025-04-30 09:28:32 +0200

    fix: update payload structure

--- src/subscriber/callbacks.py
+++ src/subscriber/callbacks.py
@@ -2,23 +2,21 @@ import json
 from cloud.sender import send_to_cloud
 
 def on_connect(client, userdata, flags, result_code, properties):
-    client.subscribe("edge/+/sensors/light")
+    client.subscribe("edge/+/sensors/+")
 
 def on_message(client, userdata, msg):
     topics = msg.topic.split('/')
 
     device_type = topics[0]
     device_id = topics[1]
+    sensor_type = topics[3]
     
-    json_object = json.loads(msg.payload.decode('utf-8'))
-
-
+    edge_json_data = json.loads(msg.payload.decode('utf-8'))
 
     payload = {
             'device_id': device_id,
-            'sensors': {
-                'temperature': json_object['value']
-            }
+            'sensor_type': sensor_type,
+            'value': json_object['value']
     }
 
     print(payload)
